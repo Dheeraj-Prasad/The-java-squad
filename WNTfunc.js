756dd142ec05a54c417e8687d7a76d627225de10
olamifuyi <fuyi456@my.yorku.ca>
2022-11-29 18:43:14 -0500

    eas to easy

---
+++ WNTfunc.js
@@ -0,0 +1,11 @@
+function easy_NUM(){
+    alert("You have chosen easy mode...guess a number between 1-20 in 3 chances or less, GOOD LUCK!");
+}
+
+function med_NUM(){
+    alert("You have chosen medium mode...guess a number between 1-50 in 6 chances or less, GOOD LUCK!");
+}
+
+function hard_NUM(){
+    alert("You have chosen hard mode...guess a number between 1-100 in 10 chances or less, GOOD LUCK!");
+}
