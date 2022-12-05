216dd6c81473fbf2a1e17574221e567b3f019cc6
olamifuyi <92198199+olamifuyi@users.noreply.github.com>
2022-12-05 03:41:45 +0000

    js changes

--- WNTfunc.js
+++ WNTfunc.js
@@ -1,22 +1,28 @@
-let guesses= 3;
-let max = 3;
+let guesses= 0;
+let max = 0;
 
 function easy_NUM(){
-    alert("You have chosen easy mode...guess a number between 1-20 in 3 chances or less, GOOD LUCK!");
+  
     guesses = 3;
     max = 20;
+    window.location.href = "playerpage.html";
+    alert("You have chosen easy mode...guess a number between 1-20 in 3 chances or less, GOOD LUCK!");
 }
 
 function med_NUM(){
-    alert("You have chosen medium mode...guess a number between 1-50 in 6 chances or less, GOOD LUCK!");
+    
     guesses = 6;
     max = 50;
+    window.location.href = "playerpage.html";
+    alert("You have chosen medium mode...guess a number between 1-50 in 6 chances or less, GOOD LUCK!");
 }
 
 function hard_NUM(){
-    alert("You have chosen hard mode...guess a number between 1-100 in 10 chances or less, GOOD LUCK!");
+    
     guesses = 10;
     max = 100;
+    window.location.href = "playerpage.html";
+    alert("You have chosen hard mode...guess a number between 1-100 in 10 chances or less, GOOD LUCK!");
 }
 
 
